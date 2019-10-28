c39a13c9767f855b9ee5f14e6ef71c1cca221c36
HHHHHG <2894700792@qq.com>
2019-10-28 12:27:01 +0800

    Create TweetApplicationTests.java

---
+++ tweet/src/test/java/com/ggemo/tweet/TweetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggemo.tweet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
