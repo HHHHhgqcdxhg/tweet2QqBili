0c56c5dd7a0afd6800e8f337bd66d9a6a2706642
HHHHHG <2894700792@qq.com>
2019-11-15 18:36:11 +0800

    statusWrapper的tostring

--- tweet/src/main/java/com/ggemo/tweet/common/StatusWrapper.java
+++ tweet/src/main/java/com/ggemo/tweet/common/StatusWrapper.java
@@ -4,6 +4,7 @@ import com.ggemo.tweet.common.util.Image;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import twitter4j.Status;
 
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.List;
 @Getter
 @Setter
 @AllArgsConstructor
+@ToString
 public class StatusWrapper {
     Status status;
     String transed;
